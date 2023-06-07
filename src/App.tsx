fa13f527e932e56cae06e8244452cd524c51e744
Elizaveta Gorbatova <gev@d-element.ru>
2023-06-07 15:57:37 +0500

    7 Организация стилей. Добавляем темы добавили светлую и темную темы с помощью изменения css переменных, добавили контекст для доступа к используемой теме по всему приложению, написали свой хук для переключения тем

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import "./styles/index.scss"
 import {Routes, Route, Link} from "react-router-dom";
-import {Suspense} from "react";
-import "./index.scss"
+import {Suspense, useContext, useState} from "react";
 import {AboutPageAsync} from "./pages/aboutPage/aboutPage.async";
 import {MainPageAsync} from "./pages/mainPage/mainPage.async";
+import {useTheme} from "./theme/useTheme";
 
 export const App = () => {
-    return <div className="app">
+    const {theme, toggleTheme} = useTheme()
+
+    return <div className={`app ${theme}`}>
+        <button onClick={toggleTheme} >Toggle theme</button>
         <Link to={"/"}>MainPage</Link>
         <Link to={"/about"}>AboutPage</Link>
         <Suspense fallback={<div>Loading...</div>}>
