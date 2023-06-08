887e83e7ea5a5f49861a79395ceec362411b04d2
Elizaveta Gorbatova <gev@d-element.ru>
2023-06-08 11:52:41 +0500

    6 Роутинг Code splitting Lazy Suspence 7 Организация стилей. Добавляем темы 8 classNames создаем git репозиторий

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,14 @@
 import {render} from "react-dom";
-import {App} from "./App";
+import App from "./App";
+import {BrowserRouter} from "react-router-dom";
+import ThemeProvider from "./theme/ThemeProvider";
+
 
 render(
-    <App />,
-    document.querySelector("#root")
+  <BrowserRouter>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </BrowserRouter>,
+  document.getElementById('root')
 )
