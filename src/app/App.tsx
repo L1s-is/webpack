dd5c842ee8103cdf0ec067145a960cfc15643e41
Elizaveta Gorbatova <gev@d-element.ru>
2023-08-07 22:55:59 +0500

    5 i18n Интернационализация. Define plugin. Плагин для переводов добавила кнопку переключения языка, ключи для перевода текста ру/англ, блок для переключателей в сайдбаре, DefinePlugin в сборку для возможности использования глобальных переменных в проекте

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,15 +4,18 @@ import {classNames} from "shared/lib/classNames/classNames";
 import {AppRouter} from "app/providers/router";
 import {NavBar} from "widgets/navbar";
 import {Sidebar} from "widgets/sidebar";
+import {Suspense} from "react";
 
 export const App = () => {
     const {theme} = useTheme()
 
     return <div className={classNames("app", {}, [theme])}>
-        <NavBar />
-        <div className="content-page">
-            <Sidebar />
-            <AppRouter />
-        </div>
+        <Suspense fallback={"..."}>
+            <NavBar />
+            <div className="content-page">
+                <Sidebar />
+                <AppRouter />
+            </div>
+        </Suspense>
     </div>
 }
--- src/widgets/sidebar/ui/Sidebar.tsx
+++ src/widgets/sidebar/ui/Sidebar.tsx
@@ -2,6 +2,7 @@ import cls from "./Sidebar.module.scss";
 import {classNames} from "shared/lib/classNames/classNames";
 import {useState} from "react";
 import {ThemeSwitcher} from "widgets/themeSwitcher";
+import {LangSwitcher} from "widgets/langSwitcher";
 
 interface SidebarProps {
     className?: string
@@ -16,7 +17,11 @@ export const Sidebar = ({className}: SidebarProps) => {
     return (
         <div className={classNames(cls.Sidebar, {[cls.isCollapsed]: isCollapsed}, [className])}>
             <button onClick={handleClick}>toggle</button>
-            <ThemeSwitcher />
+            <div className={classNames(cls.Switchers, {}, [className])}>
+                <ThemeSwitcher />
+                <LangSwitcher />
+            </div>
+
         </div>
     );
 };
