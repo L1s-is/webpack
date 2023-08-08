aed2d9834adcbb7416b51c5d6d43d441d3a1da61
Elizaveta Gorbatova <gev@d-element.ru>
2023-08-08 16:37:39 +0500

    17 Babel. Extract plugin добавила babel и i18next-extract

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -3,6 +3,25 @@ import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import {BuildOptions} from "./types/config";
 
 export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
+    const babelLoader = {
+        test: /\.(js|jsx|tsx)$/i,
+        exclude: /node_modules/,
+        use: {
+            loader: "babel-loader",
+            options: {
+                presets: ['@babel/preset-env'],
+                "plugins": [
+                    ["i18next-extract", {
+                        "nsSeparator": "~",
+                        locales: ["ru", "en"],
+                        /*keyAsDefaultValue: true,*/
+                    }],
+                    // […] your other plugins […]
+                ]
+            }
+        }
+    }
+
     const typescriptLoader = {
         test: /\.tsx?$/,
             use: 'ts-loader',
@@ -44,6 +63,7 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
     }
 
     return [
+        babelLoader,
         typescriptLoader,
         styleLoader,
         svgLoader,
